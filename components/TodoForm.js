fe3585321e571cb935c0625f5feb80c0854aa76e
vibhor <vibhorsxn@gmail.com>
2021-04-11 23:45:43 +0530

    Destructure the props

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -3,38 +3,42 @@ import { v4 as uuidv4 } from "uuid";
 import { Button, TextField } from "@material-ui/core";
 
 function TodoForm({ addTodo }) {
-    const [todo, setTodo] = useState({
-        id: "",
-        task: "",
-        completed: false
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  const changeTextField = (e) => {
+    setTodo({
+      ...todo,
+      task: e.target.value,
     });
+  };
 
-    const changeTextField = (e) => {
-        setTodo({
-            ...todo,
-            task: e.target.value,
-        })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuidv4() });
+      //Reset task form
+      setTodo({ ...todo, task: "" });
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (todo.task.trim()) {
-            addTodo({ ...todo, id: uuidv4() });
-            //Reset task form
-            setTodo({ ...todo, task: "" });
-        }
-    }
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="TodoForm">
-                <TextField label="task" name="task"
-                    type="text"
-                    placeholder="Enter your task"
-                    onChange={changeTextField} value={todo.task}></TextField>
-                <Button type="submit">Add Task</Button>
-            </form>
-        </>
-    )
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="TodoForm">
+        <TextField
+          label="task"
+          name="task"
+          type="text"
+          placeholder="Enter your task"
+          onChange={changeTextField}
+          value={todo.task}
+        ></TextField>
+        <Button type="submit">Add Task</Button>
+      </form>
+    </>
+  );
 }
 
 export default TodoForm;
